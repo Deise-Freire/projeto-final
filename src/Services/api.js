334cf0eddaf8d4f303b79d751b56e0d0f5c6b71f
Deise Freire <116744581+Deise-Freire@users.noreply.github.com>
2022-12-25 21:49:24 -0300

    Update api.js

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,13 +1,22 @@
+//import axios from 'axios';
+//import { getToken } from './utils';
+
+//export const api = axios.create({
+//  baseURL: 'http://localhost:8000',
+//});
+
+//api.interceptors.request.use(function (config) {
+//  const token = getToken();
+//  config.headers.Authorization = token ? `Token ${token}` : null;
+
+//  return config;
+//});
+
 import axios from 'axios';
-import { getToken } from './utils';
 
-export const api = axios.create({
-  baseURL: 'http://localhost:8000',
+const instance = axios.create({
+  baseURL: process.env.NODE_ENV === 'development'
+  ? 'http://localhost:3333' : 'https://api.example.com'
 });
 
-api.interceptors.request.use(function (config) {
-  const token = getToken();
-  config.headers.Authorization = token ? `Token ${token}` : null;
-
-  return config;
-});
+export default instance;
