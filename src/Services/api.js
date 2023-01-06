7d2471f56a2ba85021415ef9567f1a3127316d68
Deise Freire <116744581+Deise-Freire@users.noreply.github.com>
2023-01-06 17:09:18 -0300

    Update api.js

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { getToken } from './utils';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  //baseURL: 'http://localhost:8000', 
+  baseURL: 'http://kalitadeise2.pythonanywhere.com/'
 });
 
 api.interceptors.request.use(function (config) {
